87a9b394f2df6f6f31e155bb5fc7a24edd0fd6f3
georgekutty joseph <georgekuttyjosephgkj@gmail.com>
2022-10-12 21:11:15 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
+import { Octokit } from "@octokit/core";
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FaHistory } from 'react-icons/fa';
+const octokit = new Octokit({ auth: `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` });
+const countInitialValue = 10;
+
+function FetchData(callback) {
+  octokit.request("/repos/georgekuttyjosephgkj/git_history_app/commits", { sha: "master" })
+    .then((data) => {
+      callback(data.data)
+    })
+}
 
 function App() {
+  const [commitData, setCommitData] = useState([])
+  const [count, setCount] = useState(countInitialValue)
+  useEffect(() => {
+    FetchData((res) => {
+      setCommitData(res)
+    })
+  }, [])
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(count - 1)
+      if (count <= 0) {
+        FetchData((res) => {
+          setCommitData(res)
+        })
+        setCount(countInitialValue)
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React gk
-        </a>
-      </header>
+      <div className="header">
+        <div className="headingContainer">
+          <div className="headingContainer_left">
+            <div className="heading">GitCommitHistoryApp</div>
+            <div className="quote">designedAndDevelopedByGeorge</div>
+          </div>
+          <div className="headingContainer_right">
+            <div className="counter animate__animated animate__bounce">{count}</div>
+            <div className="iconDiv"><FaHistory /></div>
+          </div>
+        </div>
+      </div>
+      <div className="Content">
+        {commitData.map((item) => {
+          return (<div>{item["commit"]["message"]}</div>)
+        })}
+      </div>
     </div>
   );
 }
